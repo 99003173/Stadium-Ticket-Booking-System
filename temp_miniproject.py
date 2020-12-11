4052b8a64b607e8284e9cbafa07a79ce6b82534e
99003173 <75420543+99003173@users.noreply.github.com>
2020-12-11 18:39:43 +0530

    Add files via upload

--- temp_miniproject.py
+++ temp_miniproject.py
@@ -15,42 +15,41 @@ df=pd.read_csv("schedule.csv")
 # Input file to see user booking details
 bd=pd.read_csv("booking_details.csv")
 
-class user:
+class seat:
+    match_booked=0
+    tot_seat_available = 0
+    tot_seats_booked = 0
+    def seats_available(self, tot_capacity):
+        self.tot_seat_available = tot_capacity - self.tot_seats_booked
+        self.tot_seats_booked += bookings
+    
+class user(seat):
     name=""
     bookings=0
-    match_booked=0
     tot_cost=0
     def book_ticket(self,cost):
         self.bookings=int(input("Tickets to Book:"))
         tot_cost=cost*self.bookings
         print("Total cost for booking :",tot_cost)
-        return self.bookings
-
+        
 u1=user()
 
 class stadium:
-    tot_capacity=0
-    tot_seat_available=0
-    tot_seats_booked=0
-    cost=1000
+    tot_capacity = 0
+    cost = 1000
 
     def details(self,tot_capacity):
         self.tot_capacity=tot_capacity
 
-    def seats_available(self):
-        self.tot_seat_available=self.tot_capacity-self.tot_seats_booked
-        return self.tot_seat_available
-
     def match_schedule(self):
         print("\nSchedule is")
-        print(f"1.1){df.AWAY[0]}X{df.HOME[0]} {df.FORMAT[0]} on {df.DATE[0]}")
-        print(f"1.2){df.AWAY[1]}X{df.HOME[1]} {df.FORMAT[1]} on {df.DATE[1]}")
-        print(f"1.3){df.AWAY[2]}X{df.HOME[2]} {df.FORMAT[2]} on {df.DATE[2]}")
-        u1.match_booked=float(input("Match to be booked:"))
-        print("Seat Available:",self.seats_available())
+        print(f"1){df.AWAY[0]} X {df.HOME[0]} {df.FORMAT[0]} on {df.DATE[0]}")
+        print(f"2){df.AWAY[1]} X {df.HOME[1]} {df.FORMAT[1]} on {df.DATE[1]}")
+        print(f"3){df.AWAY[2]} X {df.HOME[2]} {df.FORMAT[2]} on {df.DATE[2]}")
+        u1.match_booked=int(input("Match to be booked:"))
+        print("Seat Available:", u1.seats_available(self.tot_capacity))
         u1.name=(input("Enter Username"))
-        bookings=u1.book_ticket(self.cost)
-        self.tot_seats_booked += bookings
+        u1.book_ticket(self.cost)
         print("Seat Available:",self.seats_available())
 
 mumbai=stadium()
@@ -77,6 +76,6 @@ def match():
         chennai.match_schedule()
         match()
     else:
-        exit(0)
+        return 0
 
 match()
